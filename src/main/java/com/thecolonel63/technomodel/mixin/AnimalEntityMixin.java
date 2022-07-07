b15694a2787f485ec6454c6d0154f13747b748e7
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 18:38:02 +0300

    Mixin into the damage method

--- src/main/java/com/thecolonel63/technomodel/mixin/AnimalEntityMixin.java
+++ src/main/java/com/thecolonel63/technomodel/mixin/AnimalEntityMixin.java
@@ -1,9 +1,20 @@
 package com.thecolonel63.technomodel.mixin;
 
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnimalEntity.class)
 public class AnimalEntityMixin {
 
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+
+    }
 }
