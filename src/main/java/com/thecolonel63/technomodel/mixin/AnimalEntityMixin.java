07f906541a5d867b52d266cb81d4a8ae3bff808f
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 18:05:45 +0300

    Add mixin for AnimalEntity

---
+++ src/main/java/com/thecolonel63/technomodel/mixin/AnimalEntityMixin.java
@@ -0,0 +1,9 @@
+package com.thecolonel63.technomodel.mixin;
+
+import net.minecraft.entity.passive.AnimalEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AnimalEntity.class)
+public class AnimalEntityMixin {
+
+}
