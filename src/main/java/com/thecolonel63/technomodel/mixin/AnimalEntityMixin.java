0386cde0776d6cd40c4ecfccb5134ac165f1568d
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 18:40:53 +0300

    Check if the entity is a pig

--- src/main/java/com/thecolonel63/technomodel/mixin/AnimalEntityMixin.java
+++ src/main/java/com/thecolonel63/technomodel/mixin/AnimalEntityMixin.java
@@ -13,8 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AnimalEntity.class)
 public class AnimalEntityMixin {
 
+    @SuppressWarnings("ConstantConditions")
     @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
     public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-
+        if ((Object) this instanceof PigEntity pig) {
+            
+        }
     }
 }
