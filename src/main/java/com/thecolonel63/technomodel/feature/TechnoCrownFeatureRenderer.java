a75361f7524b3286a3afb325e5ea90f33f9798e9
Buck Turk <turkbuck@yahoo.com>
2024-06-30 12:02:55 -0500

    Update to 1.21, fix https://github.com/thecolonel63/technomodel/issues/7

--- src/main/java/com/thecolonel63/technomodel/feature/TechnoCrownFeatureRenderer.java
+++ src/main/java/com/thecolonel63/technomodel/feature/TechnoCrownFeatureRenderer.java
@@ -9,28 +9,27 @@ import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.Saddleable;
 import net.minecraft.util.Identifier;
 
-public class TechnoCrownFeatureRenderer<T extends Entity & Saddleable, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
+public class TechnoCrownFeatureRenderer<T extends Entity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
 
-    private final Identifier TEXTURE;
+    private final Identifier TEXTURE = Identifier.ofVanilla("textures/entity/pig/technocrown.png");
     private final M model;
 
-    public TechnoCrownFeatureRenderer(FeatureRendererContext<T, M> context, M model, Identifier texture) {
+    public TechnoCrownFeatureRenderer(FeatureRendererContext<T, M> context, M model) {
         super(context);
-        this.TEXTURE = texture;
         this.model = model;
     }
 
-    @SuppressWarnings("ConstantConditions")
+    @SuppressWarnings("DataFlowIssue")
+    @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         if (entity.hasCustomName() && entity.getCustomName().getString().equals("Technoblade")) {
             this.getContextModel().copyStateTo(this.model);
             this.model.animateModel(entity, limbAngle, limbDistance, tickDelta);
             this.model.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
             VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(this.TEXTURE));
-            this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+            this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
         }
     }
 }
--- src/main/java/com/thecolonel63/technomodel/mixin/PigEntityRendererMixin.java
+++ src/main/java/com/thecolonel63/technomodel/mixin/PigEntityRendererMixin.java
@@ -7,13 +7,12 @@ import net.minecraft.client.render.entity.PigEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.render.entity.model.PigEntityModel;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
+@SuppressWarnings({"unchecked", "rawtypes"})
 @Mixin(PigEntityRenderer.class)
 public abstract class PigEntityRendererMixin extends LivingEntityRenderer {
     public PigEntityRendererMixin(EntityRendererFactory.Context ctx, EntityModel model, float shadowRadius) {
@@ -22,6 +21,6 @@ public abstract class PigEntityRendererMixin extends LivingEntityRenderer {
 
     @Inject(method = "<init>(Lnet/minecraft/client/render/entity/EntityRendererFactory$Context;)V", at = @At("TAIL"))
     private void addTechnoCrownFeature(EntityRendererFactory.Context context, CallbackInfo ci) {
-        this.addFeature(new TechnoCrownFeatureRenderer(((PigEntityRenderer) (Object) this), new PigEntityModel(context.getPart(EntityModelLayers.PIG_SADDLE)), new Identifier("textures/entity/pig/technocrown.png")));
+        this.addFeature(new TechnoCrownFeatureRenderer(((PigEntityRenderer) (Object) this), new PigEntityModel(context.getPart(EntityModelLayers.PIG_SADDLE))));
     }
 }
